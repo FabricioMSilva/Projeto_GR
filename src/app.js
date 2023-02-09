5c32083b9cdfff1152128c95eb2f3cde5efb1121
FABRICIO MARTINS DA SILVA <fabriciomdasilva8@gmail.com>
2023-02-08 21:41:53 -0300

    Feito teste na api

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-c
+
+
+  fetch('https://sheetdb.io/api/v1/6kiwkpohsxc8v', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        data: [
+            {
+                'ID':"ID",
+        'MAQUINA':"MAQUINA",
+        'CONJUNTO':"CONJUNTO",
+        'MINIMO':"MINIMO",
+        'DESENHO':"DESENHO",
+        'FOTO':"FOTO",
+        'DESCRIÇÃO':"DESCRIÇÃO",
+        'DISPONIVEL':"DISPONIVEL",
+        'PRATELEIRA':"PRATELEIRA"
+            }
+        ]
+    })
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
