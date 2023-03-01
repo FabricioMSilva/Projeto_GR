65072cbf7d575cf628c0cfb2ed60f480fff6e2a9
FABRICIO MARTINS DA SILVA <fabriciomdasilva8@gmail.com>
2023-02-28 21:16:44 -0300

    uptade card

--- src/Componentes/conjuntos/card.jsx
+++ src/Componentes/conjuntos/card.jsx
@@ -6,7 +6,12 @@ const Card = () => {
     fetch('https://sheetdb.io/api/v1/vrryyqg2sfdor')
     
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((response)=> console.log(response))
+   
+
+        
+
+    });
 
     
 
@@ -15,8 +20,8 @@ const Card = () => {
         <div className="geralCard">
             <div className="imgCard"><img src="" alt="" /></div>
             <div className="InfoCard">
-                <h3>jh</h3>
-                <h4>Maquina:  {MAQUINA}Minimo : </h4>
+                <h3 id="titulo"></h3>
+                <h4>Maquina:  Minimo : </h4>
                 <h4>Descrição:</h4>
                 <h4>Desenho:</h4>
             </div>
