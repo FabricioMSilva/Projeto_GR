f1ab2483f83a7015d0ef5d13250f001a991880fb
FABRICIO MARTINS DA SILVA <fabriciomdasilva8@gmail.com>
2023-03-04 10:57:26 -0300

    card funcionando

--- src/Componentes/conjuntos/card.jsx
+++ src/Componentes/conjuntos/card.jsx
@@ -1,30 +1,44 @@
 
-import React from "react";
 import "./card.css";
+import React, { useCallback, useEffect, useState } from "react";
 
 const Card = () => {
-    
-    const dados = fetch('https://sheetdb.io/api/v1/vrryyqg2sfdor')
-    
-    .then((response) => response.json())
-    .then((response)=> console.log(response))
-    
+    const [dados, setDados] = useState([]);
+
+    const getAllData = useCallback(() => {
+        fetch("https://sheetdb.io/api/v1/vrryyqg2sfdor")
+            .then((response) => response.json())
+            .then((response) => setDados(response));
+    }, []);
+
+    useEffect(() => {
+        if (!dados.length) {
+            getAllData();
+        }
+    }, []);
+
     return (
-       
-        <div className="geralCard">
-            <div className="imgCard"><img src="" alt="" /></div>
-            <div className="InfoCard">
-                <h3 id="titulo"  value={"fffafbriccio"}></h3>
-                <h4>Maquina:  Minimo : </h4>
-                <h4>Descrição:</h4>
-                <h4>Desenho:</h4>
-            </div>
-            <div className="btnsCard">
-                <button>Detalhes</button>
-                <button>Excluir</button>
-            </div>
+        <div>
+            {dados.map((item) => (
+                <div className="geralCard">
+                    <div className="imgCard">
+                        <img src={`Foto: ${item.FOTO}`} alt="foto do conjunto" />
+                    </div>
+                    <div className="InfoCard" key={item.CONJUNTO}>
+                        <h3 id="titulo">{item.CONJUNTO}</h3>
+                        <h4>{`Maquina: ${item.MAQUINA} - Mínimo: ${item.MINIMO}`}</h4>
+                        <h4>{`Descrição: ${item.DESCRIÇÃO}`}</h4>
+                        <h4>{`Desenho: ${item.DESENHO}`}</h4>
+                        <h4></h4>
+                    </div>
+                    <div className="btnsCard">
+                        <button>Detalhes</button>
+                        <button>Excluir</button>
+                    </div>
+
+                </div>
+            ))}
         </div>
     );
-}
-
+};
 export default Card;
