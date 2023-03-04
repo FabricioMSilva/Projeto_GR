7dcdc4390db60d54f365efc8c4d02464434370d9
FABRICIO MARTINS DA SILVA <fabriciomdasilva8@gmail.com>
2023-03-04 09:48:48 -0300

    Update card.jsx

--- src/Componentes/conjuntos/card.jsx
+++ src/Componentes/conjuntos/card.jsx
@@ -3,24 +3,18 @@ import React from "react";
 import "./card.css";
 
 const Card = () => {
-    fetch('https://sheetdb.io/api/v1/vrryyqg2sfdor')
+    
+    const dados = fetch('https://sheetdb.io/api/v1/vrryyqg2sfdor')
     
     .then((response) => response.json())
     .then((response)=> console.log(response))
-   
-
-        
-
-    });
-
     
-
     return (
        
         <div className="geralCard">
             <div className="imgCard"><img src="" alt="" /></div>
             <div className="InfoCard">
-                <h3 id="titulo"></h3>
+                <h3 id="titulo"  value={"fffafbriccio"}></h3>
                 <h4>Maquina:  Minimo : </h4>
                 <h4>Descrição:</h4>
                 <h4>Desenho:</h4>
